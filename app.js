4de59a68f9c55194b07d7835faf3cf689762d4b1
Coffeeteer <tgrieshiemer@gmail.com>
2017-10-04 22:24:42 -0400

    finished lecture 102, initial project upload, server created

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+//request handler
+// Watch for incoming requests of method GET
+// to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+	res.send({ hi: 'There!' })
+});
+
+module.exports = app;
