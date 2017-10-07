e7a3cfd7ba18ab69b88f9987e984a4c0043d227d
Coffeeteer <tgrieshiemer@gmail.com>
2017-10-07 13:52:21 -0400

    complete lecture 117, created error handlers for the server side of the project

--- controllers/drivers_controllers.js
+++ controllers/drivers_controllers.js
@@ -2,10 +2,11 @@ const Driver = require('../models/Driver');
 
 module.exports = {
 
-	create(req, res) {
+	create(req, res, done) {
 		const driverProps = req.body;
 
 		Driver.create(driverProps)
-			.then(driver => res.send(driver));
+			.then(driver => res.send(driver))
+			.catch(done);
 	}
 };
