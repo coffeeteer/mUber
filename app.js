76a69083f75f5da3f26b32a89aeb1408d60d9fac
Coffeeteer <tgrieshiemer@gmail.com>
2017-10-06 22:46:43 -0400

    completed lecture 112

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber', {useMongoClient: true});
 
 app.use(bodyParser.json());
 routes(app);
