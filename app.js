17c9f83e101eec0c1dd92ec329ccd2f794c3db7b
Coffeeteer <tgrieshiemer@gmail.com>
2017-10-06 21:46:27 -0400

    completed lecture 109, the body-parse middleware

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
+
 const app = express();
 
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/muber', {useMongoClient: true});
+
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
--- controllers/drivers_controllers.js
+++ controllers/drivers_controllers.js
@@ -1,5 +1,10 @@
 module.exports = {
 	greeting(req, res) {
 		res.send({ hi: 'There!' });
+	},
+
+	create(req, res){
+		console.log(req.body);
+		res.send({ hi: 'There!'})
 	}
 };
